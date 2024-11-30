aa44b5acc23a4676a12bc31d096f204a3af71123
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-11-30 21:52:36 +0700

    Fixed no-login

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth();
-});
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth();
+// });
+
+export default clerkMiddleware(
+  async (auth: { protect: () => any }, request: any) => {
+    if (!isProtectedRoute(request)) {
+      await auth.protect();
+    }
+  }
+);
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
