f84ab8be93e96594cbd0b22697b949c80a7c4ffc
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-12-04 21:20:47 +0700

    Create question detail page

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
+
 export const QuestionsSchema = z.object({
   title: z.string().min(1).max(130),
   explanation: z.string().min(50),
   tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+});
+
+export const AnswersSchema = z.object({
+  answer: z.string().min(1),
 });
